4e550efe4f667f2013f2febcfac260d8254a92d1
Hemant2903 <82782016+hailkira29@users.noreply.github.com>
2025-04-29 13:39:17 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
-import Preloader from "./components/Preloader";    // ← or "./components/Preloader"
+import Preloader from "@/components/Preloader";    // ← or "./components/Preloader"
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
